4eb2867a568927a9dac901646856eae2abe68d22
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-17 11:36:34 +0300

    added service interface and request classes.

--- product-service/src/main/java/com/eleiatech/stockmanagement/productservice/repository/entity/Product.java
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/repository/entity/Product.java
@@ -20,7 +20,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
-    private long productId;
+    private Long productId;
 
     @Column(name = "product_name")
     private String productName;
