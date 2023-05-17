be31173619b0650741537ae50b67369566daf54c
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-17 14:18:31 +0300

    added createProduct method for controller and service.

--- product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/enums/FriendlyMessageCodes.java
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/enums/FriendlyMessageCodes.java
@@ -1,7 +1,8 @@
 package com.eleiatech.stockmanagement.productservice.exception.enums;
 
 public enum FriendlyMessageCodes implements IFriendlyMessageCode {
-    OK(1000), ERROR(1001), PRODUCT_NOT_CREATED_EXCEPTION(1500);
+    OK(1000), ERROR(1001), SUCCESS(1002),
+    PRODUCT_NOT_CREATED_EXCEPTION(1500), PRODUCT_SUCCESSFULLY_CREATED(1501);
     private final int value;
 
     FriendlyMessageCodes(int value) {
