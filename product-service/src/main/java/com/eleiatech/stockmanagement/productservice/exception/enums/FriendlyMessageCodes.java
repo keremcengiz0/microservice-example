1bc07e98ad4f621242f76d961e80b05a2f66d904
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-17 12:28:09 +0300

    added Exception and handler.

--- product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/enums/FriendlyMessageCodes.java
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/enums/FriendlyMessageCodes.java
@@ -1,8 +1,7 @@
 package com.eleiatech.stockmanagement.productservice.exception.enums;
 
 public enum FriendlyMessageCodes implements IFriendlyMessageCode {
-    OK(1000);
-
+    OK(1000), ERROR(1001), PRODUCT_NOT_CREATED_EXCEPTION(1500);
     private final int value;
 
     FriendlyMessageCodes(int value) {
--- product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/utils/FriendlyMessageUtils.java
+++ product-service/src/main/java/com/eleiatech/stockmanagement/productservice/exception/utils/FriendlyMessageUtils.java
@@ -15,7 +15,7 @@ public class FriendlyMessageUtils {
     private static final String RESOURCE_BUNDLE_NAME = "FriendlyMessage";
     private static final String SPECIAL_CHARACTER = "__";
 
-    private static String getFriendlyMessage(Language language, IFriendlyMessageCode messageCode) {
+    public static String getFriendlyMessage(Language language, IFriendlyMessageCode messageCode) {
         String messageKey = null;
 
         try {
