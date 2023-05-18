b6a9bf16b97f14c1c4b141405c4e5a62414e40b2
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-18 16:52:58 +0300

    added zuul-edge-server configs.

--- zuul-edge-server/src/main/java/com/eleiatech/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
+++ zuul-edge-server/src/main/java/com/eleiatech/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -2,7 +2,15 @@ package com.eleiatech.stockmanagement.zuuledgeserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@EnableDiscoveryClient
+@EnableEurekaClient
+@EnableZuulProxy
+@CrossOrigin(origins = "*", maxAge = 3600)
 @SpringBootApplication
 public class ZuulEdgeServerApplication {
 
