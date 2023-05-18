705b1eaef4edd03fef80213d087d602e473f980e
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-18 16:17:49 +0300

    added zuul-edge-server module.

---
+++ zuul-edge-server/src/main/java/com/eleiatech/stockmanagement/zuuledgeserver/ZuulEdgeServerApplication.java
@@ -0,0 +1,13 @@
+package com.eleiatech.stockmanagement.zuuledgeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulEdgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulEdgeServerApplication.class, args);
+	}
+
+}
