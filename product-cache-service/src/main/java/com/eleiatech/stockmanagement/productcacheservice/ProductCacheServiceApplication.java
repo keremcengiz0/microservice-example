f35047a3caccc520d6bb4385fdd7bab5d3cb3825
keremcengiz0 <keremcengiz0@hotmail.com>
2023-05-29 10:38:33 +0300

    added product-cache-service module.

---
+++ product-cache-service/src/main/java/com/eleiatech/stockmanagement/productcacheservice/ProductCacheServiceApplication.java
@@ -0,0 +1,13 @@
+package com.eleiatech.stockmanagement.productcacheservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCacheServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCacheServiceApplication.class, args);
+	}
+
+}
